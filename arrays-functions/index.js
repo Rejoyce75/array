e5383f7672b092fb89b751392b0df64df283a8bc
Ajaegbu Rejoice Oluomachi <108435453+Rejoyce75@users.noreply.github.com>
2023-03-07 14:45:03 +0100

    Add files via upload

---
+++ arrays-functions/index.js
@@ -0,0 +1,8 @@
+mutating arrays are arrays that change itself instead of returning a new array with the new infomation. They change object after the method has been used.
+
+
+The methods includes; 1. prototype.pop() 2. prototype.push() 3.prototype.shift() 4. prototype.unshift() 5. prototype.reserve()
+
+Non-mutating arrays do not change the object after the method has been used
+
+The methods include 1. prototype. sice() 2. prototype. concat() 3. prototype. map() 4. prototype. filter
